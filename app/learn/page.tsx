60d7bfda36f397825cbd93e859b9a09782e1b0aa
nadyaelvira <nadyaelvira.m@gmail.com>
2026-04-23 12:12:12 +0800

    second commit

---
+++ app/learn/page.tsx
@@ -0,0 +1,3 @@
+export default function LearnPage(){
+    return <p>Ini adalah halaman Learn!</p>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Hello Nadya cantik
           </a>
         </div>
       </main>
